20c28e4ef1b65317db2cf4abaaff87b2c07e9ef2
0ddlyoko <0ddlyokoOfficial@gmail.com>
2021-06-17 11:26:12 +0200

    [Module] Remove useless files

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script will export lib folder from EJWS and create module folder
-set -e
-
-cd target/
-unzip EJWS-*.jar "lib/*"
-mkdir "module"
-cp "/home/odoo/Desktop/Test/target/Test-1.0.jar" "module/"
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script will run EJWS
-set -e
-
-java --module-path "target/classes/lib:target/EJWS-1.0.0.0.jar" -m EJWS/me.oddlyoko.ejws.EJWS module
